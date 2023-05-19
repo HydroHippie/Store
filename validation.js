4b204de70af4c3f3b204dd2598f3a1d84c16635d
HydroHippie <134084467+HydroHippie@users.noreply.github.com>
2023-05-19 12:00:37 -0400

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,1 +1,40 @@
+function validateDataFeed(dataFeed) {
+  var errors = [];
+
+  for (var i = 0; i < dataFeed.length; i++) {
+    var product = dataFeed[i];
+
+    if (!product.hasOwnProperty('id') || product.id === '') {
+      errors.push('A required field is missing for product at index ' + i + ': id');
+    }
+
+    if (!product.hasOwnProperty('price') || product.price === '') {
+      errors.push('A required field is missing for product at index ' + i + ': price');
+    }
+
+    if (!product.hasOwnProperty('availability') || product.availability === '') {
+      errors.push('A required field is missing for product at index ' + i + ': availability');
+    }
+  }
+
+  return errors;
+}
+
+// Example usage
+var dataFeed = [
+  { id: '123', name: 'Product 1', price: '10 USD', availability: 'in stock' },
+  { id: '', name: 'Product 2', price: '', availability: 'out of stock' },
+  { id: '456', name: 'Product 3', price: '20 USD', availability: '' },
+  { name: 'Product 4', availability: 'preorder' }
+];
+
+var validationErrors = validateDataFeed(dataFeed);
+
+if (validationErrors.length > 0) {
+  for (var i = 0; i < validationErrors.length; i++) {
+    console.log(validationErrors[i]);
+  }
+} else {
+  console.log('Data feed is valid. No errors found.');
+}
 
